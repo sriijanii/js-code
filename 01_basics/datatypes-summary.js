50ae260bc3effb5f7460dfaa631909b3b157e943
sriijanii <122852539+sriijanii@users.noreply.github.com>
2025-07-24 13:28:39 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+//Primitive ( call by value)
+
+// 7 types : String, Number, Boolean, null, undefined, symbol(to make value unique), BigInt
+
+const score = 688
+const scoreValue = 688.7
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;//undefined
+
+const id = Symbol('244')
+const anotherId = Symbol('244')
+console.log(id === anotherId);
+
+const bigNum = 67779999097969797n
+
+
+//Non Primitive Datatype (reference type)
+//Array, Objects, Functions
+
+const heros = ["afgs", "gshgh", "shsj"]
+{
+    name: "hgvhi"
+    age: 56
+}
+
+const myFunction = function(){
+    console.log("hii");
+}  
+
+console.log(typeof "srija");
