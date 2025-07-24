b753c6b1265b4d0d46752930eba6fd12e14c6139
sriijanii <122852539+sriijanii@users.noreply.github.com>
2025-07-24 15:49:02 +0000

    nums_maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score = 500
+//console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(typeof 'balance');
+// console.log(balance.toFixed(3));
+
+const otherNum = 56.898
+// console.log(otherNum.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//*********Maths*********** */
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(8.6));
+// console.log(Math.ceil(4.8));
+// console.log(Math.floor(3.4));
+// console.log(Math.min(4,8,1,2));
+
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
