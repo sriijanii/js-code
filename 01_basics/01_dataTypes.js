1ee3cb8e0a69440c6c2af821d7ce92d7d6b6bc3e
sriijanii <122852539+sriijanii@users.noreply.github.com>
2025-07-24 05:27:42 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as new version
+// alert( 3 + 3)
+
+// console.log(3 + 3);console.log("hi")//no code readability
+
+let name = "Dyag"//string
+let age = 12 //number
+let isLoggedIn = true //boolean
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/ false
+//null =>standalone value
+// undefined
+//symbol
+
+//object
+
+console.log(typeof "srija");
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14346
+let accountEmail = "dfg@gmail.com"
+var accountPassword = "5674v"
+accountCity = "Delhi"
+
+// accountId = 678896//not allowed
+
+
+accountEmail = "dgfgf@gmail.cpom"
+accountPassword = "hghgh"
+accountCity = "WB"
+let accountState;
+console.log(accountId);
+
+/*
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
