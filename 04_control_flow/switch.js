68b2b7ded34792e1123bf045807c84ab753958b3
sriijanii <122852539+sriijanii@users.noreply.github.com>
2025-07-25 13:38:16 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch(key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch(month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+    case "june":
+        console.log("June");
+        break;
+    default:
+        console.log("No moth matched");
+        break;
+        
+}
