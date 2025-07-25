508c4514857782a7311ad83a697d747d4f410cfa
sriijanii <122852539+sriijanii@users.noreply.github.com>
2025-07-25 14:26:20 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+//while
+// let i = 0
+// while (i <= 5) {
+//     console.log(`Value of index is ${i}`);
+//     i++;
+    
+// }
+
+// let myArr = ["jhjhk", "ghj", "fgch", "gfdf", "dxx"]
+// let arr = 0
+// while(arr < myArr.length){
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr++; 
+// }
+
+
+//***********DO-WHILE */
+let score = 117
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 12);
