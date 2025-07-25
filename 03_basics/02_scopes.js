3b27dae7f96f4aee26722a16abc4254d96e277cc
sriijanii <122852539+sriijanii@users.noreply.github.com>
2025-07-25 07:27:43 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,56 @@
+//var c = 800
+let a = 500
+
+if(true){
+    let a = 10
+    const b = 90
+    //console.log("INNER : ", a);
+}
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];   
+// }
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "iuihj"
+
+    function two(){
+        const website = "yt"
+        console.log(username);
+        
+    }
+    //console.log(website);
+    //two()
+}
+//one()
+
+if(true) {
+    username = "srija"
+    if (username == "srija"){
+        const website = " yt"
+        //console.log(username + website);
+        
+    }
+   // console.log(website);
+    
+}
+//console.log(username);
+
+
+//********************/
+console.log(addOne(5))
+
+function addOne(num){
+    return num + 1
+}
+
+
+console.log(addTwo(5))
+const addTwo = function(num){//variable 
+    return num + 2
+}
+
